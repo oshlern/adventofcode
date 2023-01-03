ef197c045634fafb14bae19d32d6c37250f19990
Osher Lerner <osherler@gmail.com>
2023-01-03 01:54:33 -0800

    nx visualization, aug path

--- foobar/bananas.py
+++ foobar/bananas.py
@@ -1,6 +1,8 @@
 from collections import defaultdict
 import networkx as nx
 import matplotlib.pyplot as plt
+import random
+from networkx.drawing.nx_pydot import graphviz_layout
 # def solution(banana_list):
 #     graph = []
 infs = set()
@@ -75,10 +77,13 @@ def ancestors(v, roots):
     return v_to_root
 
 def find_augmented(n, vs, edges, pairs): # sets
-    G2 = nx.Graph()
+    vs = set(vs)
+    edges = set(edges)
+
     blossoms = dict()
+    b_roots = dict()
+    b_membership = dict()
     exposed = vs - set([pair[0] for pair in pairs] + [pair[1] for pair in pairs])
-     # 0
     roots = {v: meta_root for v in exposed}
     F = {v for v in exposed}
     next_vs = {v for v in exposed}
@@ -91,13 +96,24 @@ def find_augmented(n, vs, edges, pairs): # sets
     for e in pairs:
         pair[e[0]] = e[1]
         pair[e[1]] = e[0]
-    print(pairs)
-    print(roots)
-    G2.add_nodes_from(list(exposed))
+#     print("NBS", nbs)
 
+    G2 = nx.Graph()
+    G2.add_nodes_from(list(exposed))
+#     pos = graphviz_layout(G2, prog="dot")
+#     nx.draw(G2, pos, with_labels=True, font_weight='bold',node_size=100,font_size=28,
+#             node_color='#5fa8d4',edge_color=[G2[u][v]['color'] for u,v in G2.edges()])
+#     plt.show()
+    
     while next_vs:
         v = next_vs.pop() # breadth better than depth
-        for w in nbs[v]:
+        next_ws = {a for a in nbs[v]}
+        while next_ws:
+            pos = graphviz_layout(G2, prog="dot")
+            nx.draw(G2, pos, with_labels=True, font_weight='bold',node_size=100,font_size=28,
+                    node_color='#5fa8d4',edge_color=[G2[u][v]['color'] for u,v in G2.edges()])
+            plt.show()
+            w = next_ws.pop()
             if not w in F:
                 x = pair[w]
                 F.add(x)
@@ -106,14 +122,9 @@ def find_augmented(n, vs, edges, pairs): # sets
                 roots[x] = w
                 next_vs.add(x)
                 even_depths.add(x)
-                G2.add_edge(v, w)
-                G2.add_edge(w, x)
-                subax1 = plt.subplot(121)
-                nx.draw(G, with_labels=True, font_weight='bold')
-                subax2 = plt.subplot(122)
-                nx.draw(G2, with_labels=True, font_weight='bold')
-                # plt.waitforbuttonpress()
-                plt.show()
+                G2.add_edge(v, w, color="black")
+                G2.add_edge(w, x, color="blue")
+
             else:
                 if not w in even_depths:
                     pass
@@ -124,22 +135,67 @@ def find_augmented(n, vs, edges, pairs): # sets
                         path = list(reversed(v_to_root)) + w_to_root # ish
                         print("___ blossoms")
                         print(blossoms)
+                        print(b_roots)
+                        print("PATH", path)
+                        for i in reversed(range(len(path)-1)):
+                            if path[i] in blossoms:
+#                                 b_root = b_roots[path[i]]
+                                b_root = path[i]
+                                next_v = path[i+1]
+                                ns = set()
+                                for e1 in edges:
+                                    if next_v in e1:
+                                        ns |= set(e1)
+                                ns.remove(next_v)
+                                blossom_path = []
+                                for nbr in ns:
+                                    blossom_path = []
+                                    head = nbr
+                                    print(i, "Head,", head)
+                                    while head != meta_root:
+                                        if head == b_root:
+                                            print(blossom_path)
+                                            break
+                                        blossom_path.append(head)
+                                        head = roots[head]
+                                        print(head)
+                                    else:
+                                        continue
+                                    break
+                                print(blossom_path)
+                            
+                                path = path[:i+1] + list(reversed(blossom_path)) + path[i+1:]
                         return path
                     else:
-                        i = 1
-                        while v_to_root[-i] == w_to_root[-i] and i < len(v_to_root) and i < len(w_to_root):
-                            # print(i, v_to_root[-i], w_to_root[-i])
-                            i += 1
-                        common_ancestor = v_to_root[-i+1]
-                        even = i % 2 == 0
-                        cycle = list(reversed(v_to_root[:-i+2])) + w_to_root[:-i+2]
-                        # cycle_vs = set(cycle)
-                        # n = 1000
-                        newV = n+1
+                        root_to_v = list(reversed(v_to_root))
+                        root_to_w = list(reversed(w_to_root))
+                        for i in range(min(len(root_to_v),len(root_to_w))):
+                            if root_to_v[i] != root_to_w[i]:
+                                i -= 1
+                                break
+
+                        common_ancestor = root_to_v[i]
+        #                     even = i % 2 == 0
+                        cycle = root_to_v[i:] + w_to_root[:-i-1]
+                        newV = common_ancestor
+#                         n += 1
+                        print("CYCLE")
+                        print(root_to_v[i], root_to_w[i], v_to_root[-i-1], w_to_root[-i-1], "__", i, v_to_root, w_to_root)
+                        print(cycle, newV)
                         blossoms[newV] = cycle
-                        roots[newV] = roots[common_ancestor]
-                        # for v in cycle:
-                        #     roots[v] = roots[common_ancestor]
+                        b_roots[newV] = b_roots[common_ancestor] if common_ancestor in b_roots else common_ancestor
+#                         for V1 in cycle:
+#                             if V1 in blossoms:
+                                
+#                             b_membership[V1] = newV
+                        
+#                         roots[newV] = roots[common_ancestor]
+#                         for v1 in cycle[1:]:
+#                             del roots[v1]
+        #                         roots[v] = roots[common_ancestor]
+                        for v1 in roots:
+                            if not v1 in cycle and roots[v1] in cycle:
+                                roots[v1] = newV
                         F = F.difference(cycle)
                         F.add(newV)
                         # next_vs = next_vs.difference(cycle)
@@ -149,29 +205,38 @@ def find_augmented(n, vs, edges, pairs): # sets
                         if common_ancestor in even_depths: #if i % 2 == 0:
                             even_depths.add(newV)
                         even_depths = even_depths.difference(cycle)
-                        for v1 in cycle:
+                        to_del = next_ws - nbs[v]
+                        for v1 in cycle[1:]:
+#                             print("Nbs", v1, nbs[v1])
                             for v2 in nbs[v1]:
-                                if v2 == newV:
+#                                 print(v1, v2, nbs[v1], nbs[v2], newV, cycle)
+                                if v2 in cycle[1:]:
                                     continue
                                 nbs[v2].remove(v1)
+                                if v2 == newV:
+                                    continue
                                 nbs[v2].add(newV)
+                                nbs[newV].add(v2)
                             del nbs[v1]
-    return False
-
+                        print("AAAAAA"*3)
+                        V0 = cycle[0]
+                        for V1 in cycle[1:]:
+                            G2 = nx.contracted_nodes(G2, V0, V1)
+                        G2 = nx.relabel_nodes(G2, {V0: newV})
+                        print(v, next_ws)
+                        v = newV
+                        next_ws = nbs[newV].difference(cycle)
+                        print(v, next_ws)
+        pos = graphviz_layout(G2, prog="dot")
+        nx.draw(G2, pos, with_labels=True, font_weight='bold',node_size=100,font_size=28,
+            node_color='#5fa8d4',edge_color=[G2[u][v]['color'] for u,v in G2.edges()])
+        plt.show()
+        return False
 
 banana_list = [1, 7, 3, 21, 13, 19]
-# banana_list = [random.randint(1,int(10737418230/3)) for _ in range(4)]
-
 n = len(banana_list)
 vs = list(range(n))
-# edges = [(i,j) for i in range(n) for j in range(i+1, n) if infinite(banana_list[i], banana_list[j])]
-edges = []
-for i in range(n):
-    for j in range(i+1, n):
-        # print(i,j,banana_list[i],banana_list[j])
-        if infinite(banana_list[i], banana_list[j]):
-            # print("+")
-            edges.append((i,j))
+edges = [(i,j) for i in range(n) for j in range(i+1, n) if infinite(banana_list[i], banana_list[j])]
 
 candidates = [e for e in edges]
 pairs = []
@@ -179,86 +244,22 @@ while candidates:
     pair = candidates.pop()
     pairs.append(pair)
     candidates = [e for e in candidates if not (e[0] in pair or e[1] in pair)]
-print(vs)
+# print(vs)
 print(edges)
 print(pairs)
 G = nx.Graph()
 for e in edges:
     w = 3 if e in pairs else 1
-    G.add_edge(*e, weight=w)
+    c = "blue" if e in pairs else "black"
+    G.add_edge(*e, weight=w, color=c)
 # G.add_edges_from(edges)
+print(G.edges())
+colors = [G[u][v]['color'] for u,v in G.edges()]
+weights = [G[u][v]['weight'] for u,v in G.edges()]
+pos = nx.circular_layout(G)
+
+nx.draw(G, pos, edge_color=colors, width=weights, with_labels=True,node_size=1000,font_size=28,node_color='#5fa8d4')
 print("-"*10)
-print(find_augmented(n, set(vs), set(edges), set(pairs)))
-nx.draw(G, with_labels=True, font_weight='bold')
+# nx.draw(G,  font_weight='bold')
 plt.show()
-# print(edges)
-# print(inf/tot, inf, tot)
-# print(len(banana_list), len(edges), len(banana_list)*(len(banana_list)-1)/2)
-
-
-# def find_augmented(vs, nbs, edges, pairs): # sets
-#     root = -1 # 0
-#     forest = {}
-#     F_dict = {}
-#     F_depth = {}
-#     even_depths = set()
-#     odd_depths = set()
-#     unmarked_edges = edges - pairs
-#     unmarked_vs = set([v for v in vs])
-#     unmarked_F_vs = set()
-#     exposed = vs - set([pair[0] for pair in pairs] + [pair[1] for pair in pairs])
-#     for v in exposed:
-#         subtree = {}
-#         forest[v] = subtree #.append(subtree)
-#         F_dict[v] = subtree
-#         F_depth[v] = 0
-#         unmarked_F_vs.add(v)
-#     # next_vs = even_depths.intersection(unmarked_vs)
-#     next_vs = exposed
-#     while next_vs: 
-#         v = next_vs.pop() # breadth better than depth
-#         for w in nbs[v]:
-#             if not w in F:
-#                 x = pair(w)
-#                 add(v, w)
-#                 add(w, x)
-#                 next_vs.add(w)
-                
-#             else:
-#                 if not w in even_depths:
-#                     pass
-#                 else:
-#                     # head = v
-#                     # v_to_root = []
-#                     # while head != root:
-#                     #     v_to_root.append(head)
-#                     #     head = roots[head]
-#                     # head = w
-#                     # root_to_w = []
-#                     # while head != root:
-#                     #     if head in v_to_root:
-                            
-#                     #     root_to_w.insert(0, head) # deque
-#                     #     head = roots[head]
-
-
-#                     roots[v
-#                     v_root, v_to_root = [] # reverse during construction
-#                     w_root, w_to_root = []
-#                     path = path = list(reversed(v_to_root)) + [(v,w)] + w_to_root
-#                     if path[0] != path[-1]:
-#                         return path
-#                     else:
-                        
-#                         Gp, Mp = contract_blossom(G,M,B)
-#                         Pp = find_augmented(Gp, Mp)
-#                         P = lift(Pp)
-#                         return P
-        # unmarked_vs.remove(v)
-        # next_vs = even_depths.intersection(unmarked_vs)
-    # return False
-
-
-
 # print(solution([1, 7, 3, 21, 13, 19]))
-# print(solution(4))
