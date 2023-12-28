a91f65438024b754845aba323200a45b87401036
Osher Lerner <osherler@gmail.com>
2023-12-28 09:24:53 -0800

    day 6 in C

--- advent23/06/c_solution2.c
+++ advent23/06/c_solution2.c
@@ -9,12 +9,12 @@ const int N_TRIALS = 4;
 // const char* FILENAME = "test";
 // const int N_TRIALS = 3;
 
-int score_trial(int N, int best) {
-    int cur = 0;
-    for (int i = 0; i < N/2; i++) {
+long score_trial(long N, long best) {
+    long cur = 0;
+    for (long i = 0; i < N/2; i++) {
         cur += (-i) + (N-1-i);
         if (cur > best) {
-            int n_ways = (N/2 - i) * 2;
+            long n_ways = (N/2 - i) * 2;
             if (N%2 == 0)
                 n_ways -= 1;
             return n_ways;
@@ -23,7 +23,8 @@ int score_trial(int N, int best) {
     return 0;
 }
 
-void remove_spaces(char* s) {
+// from https://stackoverflow.com/a/1726321
+void remove_spaces(char* s) { 
     char* d = s;
     do {
         while (*d == ' ') {
@@ -43,41 +44,20 @@ int main(int argc, char *argv[]) {
     strtok(line, ":");
     char* time_str = strtok(NULL, "\n");
     remove_spaces(time_str);
-    int time = atoi(time_str);
+    long time = strtol(time_str, NULL, 10);
 
     getline(&line, &len, fp);
     strtok(line, ":");
     char* dist_str = strtok(NULL, "\n");
     remove_spaces(dist_str);
-    int dist = atoi(dist_str);
-    // int dist;
-    // getline(&line, &len, fp);
-    // strtok(line, ":");
-    // // sscanf(strtok(NULL, "\n"), "%d", &dist);
-    // dist = atoi(strtok(NULL, "\n"), NULL, 10);
+    long dist = strtol(dist_str, NULL, 10);
 
-    printf("a %d %d\n", time, dist);
-    int score = score_trial(time, dist);
+    printf("time %ld dist %ld\n", time, dist);
+    long score = score_trial(time, dist);
 
-
+    printf("score: %ld\n", score);
 
     fclose(fp);
     free(line);
     exit(EXIT_SUCCESS);
 }
-
-
-
-// n chose i
-// n!/i!*(n-i)!
-// 1
-// i=0
-// * (n-i/i+1)
-// 0*7
-// 1*6
-// 2*5
-// 3*4
-
-// 
-// -i + n-i-1
-// 
