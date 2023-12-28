4e3fa4d595879a6d50abbf8f4313f3199e970026
Osher Lerner <osherler@gmail.com>
2023-12-28 09:04:03 -0800

    day 6 puzzle 1 in C

--- advent23/06/c_solution.c
+++ advent23/06/c_solution.c
@@ -4,10 +4,59 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// const char* FILENAME = "input";
-// const int N_TRIALS = 4;
-const char* FILENAME = "test";
-const int N_TRIALS = 3;
+const char* FILENAME = "input";
+const int N_TRIALS = 4;
+// const char* FILENAME = "test";
+// const int N_TRIALS = 3;
+
+int score_trial(int N, int best) {
+    int cur = 0;
+    for (int i = 0; i < N/2; i++) {
+        cur += (-i) + (N-1-i);
+        if (cur > best) {
+            int n_ways = (N/2 - i) * 2;
+            if (N%2 == 0)
+                n_ways -= 1;
+            return n_ways;
+        }
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    FILE* fp;
+    char* line = NULL;
+    size_t len = 0;
+    ssize_t read;
+    fp = fopen(FILENAME, "r");
+
+    int times[N_TRIALS];
+    getline(&line, &len, fp);
+    strtok(line, ":");
+    for (int i = 0; i < N_TRIALS; i++)
+        times[i] = atoi(strtok(NULL, " "));
+
+    int dists[N_TRIALS];
+    getline(&line, &len, fp);
+    strtok(line, ":");
+    for (int i = 0; i < N_TRIALS; i++)
+        dists[i] = atoi(strtok(NULL, " "));
+
+    int total = 1;
+    for (int i = 0; i < N_TRIALS; i++) {
+        int score = score_trial(times[i], dists[i]);
+        printf("time: %d, dist: %d, score: %d\n", times[i], dists[i], score);
+        total *= score;
+    }
+    printf("%d\n", total);
+
+
+    fclose(fp);
+    free(line);
+    exit(EXIT_SUCCESS);
+}
+
+
 
 // n chose i
 // n!/i!*(n-i)!
@@ -22,49 +71,3 @@ const int N_TRIALS = 3;
 // 
 // -i + n-i-1
 // 
-int score_trial(int N, int best) {
-    int cur = 0;
-    // for (int i = 0; i < N/2; i++) {
-    //     cur += (-i) + (N-1-i);
-    //     if (cur > best) {
-    //         int n_ways = (N/2 - i) * 2;
-    //         if (N%2 == 0)
-    //             n_ways -= 1;
-    //         return n_ways;
-    //     }
-    // }
-    return 0;
-}
-
-int main(int argc, char *argv[]) {
-    FILE* fp;
-    char* line = NULL;
-    size_t len = 0;
-    ssize_t read;
-    fp = fopen(FILENAME, "r");
-
-    char *times_str, *dists_str;
-    
-        times[i] = atoi(strtok_r(NULL, " ", &times_str));
-    printf("A %s\n", times_str);
-
-    getline(&line, &len, fp);
-    strtok_r(line, ":", &dists_str);
-    printf("A %s\n", times_str);
-    printf("B %s\n", dists_str);
-
-    int total = 1;
-    for (int i = 0; i < N_TRIALS; i++) {
-        int time = atoi(strtok_r(NULL, " ", &times_str));
-        int dist = atoi(strtok_r(NULL, " ", &dists_str));
-        int score = score_trial(time, dist);
-        // printf("time: %d, dist: %d, score: %d\n", time, dist, score);
-        // total *= score;
-    }
-    printf("%d", total);
-
-
-    fclose(fp);
-    free(line);
-    exit(EXIT_SUCCESS);
-}
