9376cf0a0ef6e7910b17d50fc3fde17e0a2119a1
Osher Lerner <osherler@gmail.com>
2024-03-23 03:32:29 -0700

    wall partition prototype, uses wall loops. Next steps would be using vertices (to fix cointersections and colinear walls)

--- leetcode/walls_partition.py
+++ leetcode/walls_partition.py
@@ -1,31 +1,41 @@
-# https://www.codewars.com/kata/60f5868c7fda34000d556f30/train/python   
+# https://www.codewars.com/kata/60f5868c7fda34000d556f30/train/python
+# from collections import namedtuple
+# Point = namedtuple("Point", "x y")
+
+
+# remove duplicates
+# trace path to see if it goes through box
+
 def has_partitions(width, height, walls):
-    
+    print(walls)
 #     Expand connected component. If found a cycle, true
 # Remove connected component, try next
     while walls:
-        q = deque(walls[0])
-        connnected = set()
-        while q:
+        w = walls[0]
+        q = deque(w)
+        sources = {w: None}
+        while q:                   #NVM if doesn't work, DFS should find all cycles through source
             v = q.popleft()
             for n in neighbors(v, walls):
-                if n in connected: return True
-                connected.add(n)
-                q.append(n)
-        walls = [w for w in walls if w not in connected]
+                if n in sources:
+                    path = reconstruct_path(v,n,sources)
+                    if not is_outside(path, width, height):
+                        return True
+                else:
+                    sources[n] = v
+                    q.append(n)
+        walls = [w for w in walls if w not in sources]
     return False
             
-        
-        if v in connected:
-        
-    path += [w]
-    ns =  neighbors(walls, w)
-    if set_intersection(path, ns):
-        return True
-    for n in ns:
-        if search(path, n, walls):
-            
-        
-    for n in:
-        
-    pass
+    
+
+    
+def neighbors(v, walls):
+    return [w for w in walls if intersect(v, w)]
+
+def intersect(v, w):
+    return ccw(v[0], w[0], w[1]) != ccw(v[1], w[0], w[1]) \
+       and ccw(w[0], v[0], v[1]) != ccw(w[1], v[0], v[1])
+
+def ccw(A, B, C): # slope AB < slope BC
+    return (B.y-A.y)(C.x-B.x) < (B.x-A.x)(C.y-B.y)
