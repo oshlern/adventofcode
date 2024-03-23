6ce65ece197650057c32bd6ecc56e8b26ee14c84
Osher Lerner <osherler@gmail.com>
2024-03-23 01:41:56 -0700

    progress on BFS

--- leetcode/grid_shortest_path.py
+++ leetcode/grid_shortest_path.py
@@ -3,20 +3,26 @@
 from preloaded import Node, print_grid # Use print_grid function to visualize a given path on the given grid. 
 from collections import deque
 
-
 def find_shortest_path(grid:list[list[Node]], start_node:Node, end_node:Node):
     if not grid: return []
-    x_len, y_len = len(grid), len(grid[0])
-    S, E = start_node.position, end_node.position
-    S, E = (S.x, S.y), (E.x, E.y)
 
-    def get_ns(x,y):
-        return [(nx,ny) for nx,ny in [(x-1,y),(x+1,y),(x,y-1),(x,y+1)] \
-            if 0<=nx<x_len and 0<=ny<y_len and grid[nx][ny].passable]
+    def get_ns(v):
+        x, y = v.position.x, v.position.y
+        # ns =  [(x-1,y),(x+1,y)] if end_node.position.x < x else [(x+1,y),(x-1,y)]
+        # ns += [(x,y-1),(x,y+1)] if end_node.position.y < y else [(x,y+1),(x,y-1)]
+        # return [grid[nx][ny] for nx,ny in ns  \
+        #     if 0<=nx<len(grid) and 0<=ny<len(grid[0]) and grid[nx][ny].passable]
+#             [(x-1,y),(x+1,y)]
+#         ns = [(x-1,y),(x+1,y),(x,y-1),(x,y+1)]
+        return [grid[nx][ny] for nx,ny in [(x-1,y),(x+1,y),(x,y-1),(x,y+1)] \
+            if 0<=nx<len(grid) and 0<=ny<len(grid[0]) and grid[nx][ny].passable]
             
-    q = deque([(0,S,[S])])
+    q = deque([(start_node,[start_node])])
+    visited = set()
     while deque:
-        d, v, path = q.popleft()
-        if v == E: return path
-        for n in get_ns(*v):
-            q.append((d+1,n,path+[grid[n[0]][n[1]]]))
+        v, path = q.popleft()
+        visited.add(v)
+        if v == end_node: return path
+        for n in get_ns(v):
+            if n not in visited:
+                q.append((n,path+[n]))
