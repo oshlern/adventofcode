90fb58997ad2c399de3df4e19bba194820d9d12d
Osher Lerner <osherler@gmail.com>
2024-03-24 13:10:01 -0700

    wheel method

--- leetcode/n_n_mod_m.py
+++ leetcode/n_n_mod_m.py
@@ -1,21 +1,28 @@
 # https://www.codewars.com/kata/638b4205f418c4ab857f2692/train/python
 import math
+from itertools import cycle
+
+base_ps = [2,3,5,7,9,11]
+wheel_m = math.prod(base_ps)
+h = [j for j in range(wheel_m) if all(j%p!=0 for p in base_ps)]
+intervals = [h[i+1]-h[i] for i in range(len(h)-1)] + [h[0]+wheel_m - h[-1]]
 
 def f(m:int) -> int:
     n = 1
     pows = {}
-    pow_k = 1
-    
-    i = 2
-    if m%i == 0:
-        pow = 0
-        while m % i == 0:
-            m //= i
-            pow += 1
-        pows[i] = pow
-        n *= i
+
+    for i in base_ps:
+        if m % i == 0:
+            pow = 0
+            while m % i == 0:
+                m //= i
+                pow += 1
+            pows[i] = pow
+            n *= i
+
+    iter = cycle(intervals)
+    i = h[0] + next(iter)
     m_rt3 = math.ceil(m**(1/3))
-    i = 3
     while i <= m_rt3:
         if m % i == 0:
             pow = 0
@@ -25,7 +32,8 @@ def f(m:int) -> int:
             m_rt3 = math.ceil(m**(1/3))
             pows[i] = pow
             n *= i
-        i += 2
+        i += next(iter)
+        
     if m != 1:
         if (m_sq := m**0.5) % 1 == 0:
             pows[m_sq] = 2
@@ -42,7 +50,7 @@ def f(m:int) -> int:
             if max_exp < exp:
                 max_exp, max_p = exp, p
         max_p = max(pows, key=lambda p: pows[p] / used_pows[p])
-        max_exp = pows[p] / used_pows[p]
+        max_exp = pows[max_p] / used_pows[max_p]
         direct_increase = math.ceil(max_exp / n)
         if direct_increase == 1: return n
         # try multiplying by pi
@@ -50,7 +58,6 @@ def f(m:int) -> int:
         n *= max_p
         next_max_exp = max(pows[p] / used_pows[p] for p in pows)
         p_increase = max_p*math.ceil(next_max_exp / n)
-        print(max_p, p_increase, direct_increase)
         if direct_increase < p_increase:
             n /= max_p
             n *= direct_increase
