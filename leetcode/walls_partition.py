bb8135655f42544a10a2616906bd948eb7305488
Osher Lerner <osherler@gmail.com>
2024-03-23 20:08:29 -0700

    prototyping wall collapse

--- leetcode/walls_partition.py
+++ leetcode/walls_partition.py
@@ -320,3 +320,107 @@ def intersection(w, u): # sort
 
 # def ccw(A, B, C): # slope AB < slope BC
 #     return (B.y-A.y)(C.x-B.x) < (B.x-A.x)(C.y-B.y)
+
+
+from collections import namedtuple
+Point = namedtuple("Point", "x y")
+
+
+
+class Graph:
+    def __init__(self):
+        self.comps = {}
+        self.es = set()
+        self.n_cycles = 0
+
+    def push_edge(self, v1, v2):
+        if (v1, v2) in self.es: return False
+        self.es.add((v1, v2))
+
+        for v in (v1, v2):
+            if not v in self.comps:
+                self.comps[v] = set([v])
+
+        if (c1 := self.comps[v1]) == (c2 := self.comps[v2]):
+            self.n_cycles += 1
+            return True
+
+        c1.update(c2)
+        for v in c2: self.comps[v] = c1
+        del c2
+        return False
+
+def has_partitions(width, height, walls):
+    borders = [
+        (Point(0, 0), Point(width,0)),
+        (Point(0, 0), Point(0,height)),
+        (Point(width, 0), Point(width, height)),
+        (Point(0, height), Point(width, height)),
+    ]
+#     [tuple(Point(x,y) for x in [0, width]) for y in [0, height]] \
+#            + [tuple(Point(x,y) for x in [0, width]) for y in [0, height]]
+    walls = [u for w in walls if (u:=truncate(w,borders,width, height))]
+    walls = sorted(set(walls + borders))
+#     print(walls)
+    
+    on_border = lambda p: (p.x==0 or p.x==width) and (p.y==0 or p.y==height)
+    
+#     Vs, Es = set(), set()
+    G = Graph()
+    for w in walls:
+        ps = [p for u in walls if u!=w and (p:=intersection(u,w))]
+        vs = [Point(0,0) if on_border(p) else p for p in ps]
+        print(vs, w)
+        vs = sorted(set(vs))
+        for v1,v2 in zip(vs, vs[1:]):# in range(1,len(vs)):
+#             print(v1, v2, Vs, Es)
+#             if (v1,v2) in Es: continue
+#             if v1 in Vs and v2 in Vs:
+#                 print(v1, v2, w, Vs)
+#                 print(Es)
+#                 return True
+#             Vs.add(v1); Vs.add(v2)
+#             Es.add((v1,v2))
+            
+
+            if G.push_edge(v1, v2):
+                return True
+#             if G.n_c Compress wall points to one "point" then find first cycle
+#     return G.n_cycles >= 2
+    return False
+
+
+def truncate(wall, borders, width, height):
+    out = [p for b in borders if (p := intersection(wall, b))]
+    out += [p for p in wall if 0<=p.x<=width and 0<=p.y<=height]
+    out = tuple(sorted(set(out)))
+    if len(out) != 2: return False
+    return out
+
+def intersection(w, u): # sort
+    (A,B), (C,D) = sorted([w, u])
+    AB = Point(A.x - B.x, A.y - B.y)
+    CD = Point(C.x - D.x, C.y - D.y)
+    BD = Point(B.x - D.x, B.y - D.y)
+    denom  = AB.y*CD.x - AB.x*CD.y
+    numer  = AB.y*BD.x - AB.x*BD.y
+    numer2 = CD.y*BD.x - CD.x*BD.y
+    if denom == 0: # parallel
+        print(numer, numer2)
+        if numer != 0 or numer2 != 0: return False # not colinear
+        # Find where C,D fall along line AB
+        magAB = AB.x*AB.x+AB.y*AB.y
+        tC = ((C.x-B.x)*AB.x + (C.y-B.y)*AB.y) # (C - B) * (A-B)   /|AB|^2
+        tD = ((D.x-B.x)*AB.x + (D.y-B.y)*AB.y) # (D - B) * (A-B)   /|AB|^2
+
+        options = [] # find all endpoints within the intersection
+        if 0<=tC<=magAB: options.append(C)
+        if 0<=tD<=magAB: options.append(D)
+        if min(tC,tD)<=0 and magAB<=max(tC,tD): options += [A,B]
+        return min(options) if options else False
+
+    t = numer / denom
+    t2 = numer2 / denom
+    if not (0<=t<=1 and 0<=t2<=1): return False
+    p = Point(D.x + t*CD.x, D.y + t*CD.y)
+    return p
