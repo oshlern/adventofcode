5099e43418e1be459a316d650131edf38cda26ae
Osher Lerner <osherler@gmail.com>
2024-03-25 17:52:51 -0700

    progress on pow tow split by p, working on p powers

--- leetcode/mod_power_tower.py
+++ leetcode/mod_power_tower.py
@@ -1,138 +1,107 @@
-def tower(base, h, m):
-    factorization = [(p, k)] # m
+import math
+
+def tower(b, h, m):
+    """Return base ** base ** ... ** base, where the height is h, modulo m. """
+    if m == 1: return 0
+    if b == 1 or h == 0: return 1
+    if h == 1: return b % m
+    return CRT([(p_tower(b, h, p, k), p**k) for p,k in factors(m)])
+
+def p_tower(b, h, p, k): # b^b^b % p^k
+    b = b % (p**k)
+    if b == 0: return 0
+    if b == 1: return 1
+    if h == 1: return b
+    if b % p == 0:
+        r = 0
+        a = b
+        while a % p == 0:
+            a //= p
+            r += 1
+#         if r >= k: return 0
+
+        e = b
+        if e*r >= k: return 0
+        for i in range(h-2):
+            e = e**b
+            if r*e >= k: return 0
+#         t_power = e*r
+#         (a * p^r) ^ e % p^k = a^e % p^(k-r*e) * p^(r*e)
+        return pow(a, e, p**(k-r*e)) * p**(r*e)
+#     k -= e*r
+#     M = p ** (r*e)
+#     a^b^b^b * M % p^k
+#     = a^b^b % p^(k-e*b_k) * M
+#     return pow(a, tower(b,h-1,(p-1)*p^(k-1)), p**k)
     
-    for p,k in factorization:
-        a = base % p
-        r = (base // p^ r)
+
+#         r = n_powers(b, p)
+#         t = 1
+#         for i in range(h):
+#             t *= b
+#             if t > thresh:
+#                 return True
+#         if gt_tower(b,h-1,k/r):
+#             return 0
+#         p *= t
+#         a = coprime, 
         
-        if r:
-            if exp >= k/r: 0
-        else:
-            e = exp % (p-1) * p^(k-1)
-            e = CRT(exp % p-1, exp % p^(k-1))
-            a ** e % p^k
-        b^e2 % p^(k-1)
-        b^b^b^b % p^k = 
-        b ^ CRT(e1 % (p-1), e1 % p^(k-1))
-        b ^ CRT(e2 % (p-1), e2 % p^(k-2))
-        e2 % p
-        e0 % p^3 = 
-        b^ CRT(e1 % (p-1), e1 % p^2)
-            b^ CRT(e2 % (p-1), e2 % p)
-        b ^ CRT(e1 % p-1, b ^ CRT(e2 % p-1, ))
-
-        e0 %p^1 = b^(e1 % p-1) % p
-
-        e0 %p^2 = b^(e1 % (p-1)p^1) % p^2
-                = b^crt(e1 % p-1, e1 % p^1) % p^2
-                = b^crt(e1 % p-1, b^(e2 % p-1) % p) % p^2
-        
-        
-        e0 %p^3 = b^e1 % p^3
-                = b^(e1 % (p-1)p^2) % p^3
-                = b^(crt(e1 % (p-1), e1 % p^2) % p^3
-                = b^(crt(e1 % (p-1), e1 % p^2) % p^3
-
-        e0 %p^2 = b^crt(e1 % p-1, b^(e2 % p-1) % p) % p^2
-        e0 %p^2 = b^(e1 % p-1, b^(e2 % p-1))
-        
-
-def p_tower(base, h, p, k): # b^b^b % p^k
-#     [(pi, ki)]= factorize(p - 1)
-
+    # coprime part
     out = 0
     for i in range(max(0,k-h), k):
-        exp = CRT(tower(b, h-k + i, p-1), p-1, out, p^i)
-        out = pow(b, exp, p^(i+1))
-                     
-    b % p^k
-    b^b^b 
+        exp = CRT([(tower(b, h-k + i, p-1), p-1), (out, p**i)])
+        print(i, h-k, exp)
+        out = pow(b, exp, p**(i+1))
+    print("p tower", b, h, p, k, out)
+    return out
 
-                     
-        
-        out = b ** CRT(tower(b, i, p-1), pow(b,out,p-1))
-    b ** CRT(tower(b, h-1, p-1),
-    
-        
-        
-        base ^ exp2
-        
-        
-        
-        
-        k -= 1
-        base 
+def gt_tower(b,h,thresh):
+    if b <= 1: return b > thresh
+    t = 1
+    for i in range(h):
+        t *= b
+        if t > thresh:
+            return True
+    return False
 
-a % p
-a^a^a^a^a
- a % p 
-a^a = a^(a % p-1)
-a^a^a = a^(a^a % p-1)
-        
-    
-    
-    
-    """Return base ** base ** ... ** base, where the height is h, modulo m. """
-    pass
-
-n % 34
-
-n % pi^ki
-
-# split into pi^ki
-for relatively prime, just need to find power % phi(n)
-ap (a^m % p) * p^m.  a^k p^k = 0
-(p-1) * k
-
-base, mod
-a * gcd(base, mod)
-a * d **exp % m
-a ** d % m   * d**exp % m
-
-a * p^r
-a ** exp % p  *  p^exp*r if exp*r > k: 0
-exp % p-1, exp > k/r
-
-a ** exp % p^r
-M = (p-1) * p^(k-1)
-exp % M
+#     if h == 0:
+#         return 1 > thresh
+#     else
 
 
+def factors(m):
+    fs = []
+    for i in range(2, math.ceil(m**0.5)):
+        k = 0
+        while m % i == 0:
+            print(i,m)
+            m //= i
+            k += 1
+        if k: fs.append((i, k))
+    if m != 1: fs.append((m, 1))
+    return fs
 
+# def factors(m):
+#     return [(p, n_powers(m,p)) for p in range(2,m) if m%p==0]
+def n_powers(m, p):
+    pow = 0
+    while m % p == 0:
+        m //= p
+        pow += 1
+    return pow
 
+# print(factors(120))
 
- 
-seqs = []
-for i in range(10):
-    x = 1
-    seqs.append([(x:= (x*i)%10) for _ in range(4)])
-    
-def is_zero(lst):
-    i = 0 # counts consequtive zeros
-    while i < len(lst) and lst[i] == 0:
-        i += 1
-    return i % 2
+def CRT(bs_ns):
+    N = math.prod(n for b,n in bs_ns)
+    return sum(b*Beizout(N//n, n)[0]*N//n for b,n in bs_ns) % N
 
-def last_digit(lst, m=10):
-    if len(lst) == 0:
-        return 1
-    b, lst = lst[0], lst[1:]
-    if is_zero(lst):                                    # e=0 --> b^e = 1
-        return 1
-    elif b == 0:
-        return 0                                        # b=0 --> b^e = 0
+def Beizout(a,b): # find x,y s.t. ax+by=gcd(a,b)
+    if b == 0: return 1, 0, a
+    q, r = divmod(a,b)
+    x, y, g = Beizout(b, r)
+    return y, x-y*q, g
 
-    if m == 10:
-        return seqs[b%10][(last_digit(lst, 4)-1)%4] # (b^e)%10 from precomputed period-4 seqs
-    elif m == 4:
-        match b % 4:                                    # logical casework
-            case 0:
-                return 0
-            case 1:
-                return 1
-            case 2:                                     # e>1 --> (2^e)%4 = 0
-                return 0 if (lst and lst[0] > 1 and not is_zero(lst[1:])) else 2
-            case 3:                                     # e%2 determines if (3^e)%4 = 3 or 1
-                return 3 if last_digit(lst, 2) else 1
-    elif m == 2:
-        return b % 2 # (b^e)%2 = (b)%2  note: ruled out e=0 earlierg
+# print(Beizout(3,17))
+# print(CRT([(1,4),(2,5),(4,7)]))
+print(p_tower(2, 3, 3, 1))
