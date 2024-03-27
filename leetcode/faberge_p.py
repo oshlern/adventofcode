18e4c9786e9c4cb2908851a91ff2bb009cd44298
Osher Lerner <osherler@gmail.com>
2024-03-27 11:38:15 -0700

    succinct faberge p

--- leetcode/faberge_p.py
+++ leetcode/faberge_p.py
@@ -1,61 +1,70 @@
 # https://www.codewars.com/kata/54cb771c9b30e8b5250011d4/train/python
-MOD = 998244353
-p = MOD
-def height(n, m):
-    print(n, m)
-    if n >= m: return pow(2, m, p) - 1
-    if n > m//2:
-        return (pow(2, m, p) - height(m-n-1, m) - 2) % p
-    out = 0
+p = 998244353
+
+def height(n, m): # doesn't work if n>p or m%p<n
+    if n >= m:   return  pow(2,m,p)-1
+    if n > m//2: return (pow(2,m,p)-2-height(m-n-1,m)) % p
     c = 1
-    c_p = 0
-    num = m+1
-    denom = 0
-    x, y = 0, 0
-    m2 = m//2 + m%2
+    return sum(c:=(c*(m-i)*pow(i+1,p-2,p)) % p for i in range(n)) % p
+
+
+# MOD = 998244353
+# p = MOD
+# def height(n, m):
+#     print(n, m)
+#     if n >= m: return pow(2, m, p) - 1
+#     if n > m//2:
+#         return (pow(2, m, p) - height(m-n-1, m) - 2) % p
+#     out = 0
+#     c = 1
+#     c_p = 0
+#     num = m+1
+#     denom = 0
+#     x, y = 0, 0
+#     m2 = m//2 + m%2
     
-#         total sum - (m-n)
-#         m2 + n-m2
+# #         total sum - (m-n)
+# #         m2 + n-m2
         
-    for i in range(1,n+1):
-        num -= 1
-        denom += 1
-#         if num % p == 0:
-#             c_p += num // p
+#     for i in range(1,n+1):
+#         num -= 1
+#         denom += 1
+# #         if num % p == 0:
+# #             c_p += num // p
+# #         else:
+#         c = c * num
+#         c, r = divmod(c, denom)
+#         if r != 0:
+#             x += 1
+#             c += (pow(denom, p-2, p) * r) #% p
 #         else:
-        c = c * num
-        c, r = divmod(c, denom)
-        if r != 0:
-            x += 1
-            c += (pow(denom, p-2, p) * r) #% p
-        else:
-            y += 1
-#         else:
-        if c >= p:
-            c = c % p
+#             y += 1
+# #         else:
+#         if c >= p:
+#             c = c % p
                 
         
-#         c = (c * num * pow(denom, p-2, p)) % p
-#             num = (num * num) % M
-#         if denom % p == 0:
-#             c_p -= denom // p
-#         else:
-#         c = (c * pow(denom, p-2, p)) % p
-#             c = (c * inv(denom, M)) % M
-#             denom = (denom * denom) % M
+# #         c = (c * num * pow(denom, p-2, p)) % p
+# #             num = (num * num) % M
+# #         if denom % p == 0:
+# #             c_p -= denom // p
+# #         else:
+# #         c = (c * pow(denom, p-2, p)) % p
+# #             c = (c * inv(denom, M)) % M
+# #             denom = (denom * denom) % M
         
-#         if c_p == 0:
-        out = (out + c) % p
+# #         if c_p == 0:
+#         out = (out + c) % p
             
-#         out
-#             c = 0
-#         else:
-#             c = (c * num * inv(denom, M)) % M
-#         c = (c * (m-i+1)) // i
-# #         m_fac *= (m-i+1)
-# #         i_fac *= i
-# #         out += m_fac // i_fac
-#         c = choose_mod(m, i, MOD)
-#         out = (out + c) % MOD
-#     print(m, n)
-    return out % p
+# #         out
+# #             c = 0
+# #         else:
+# #             c = (c * num * inv(denom, M)) % M
+# #         c = (c * (m-i+1)) // i
+# # #         m_fac *= (m-i+1)
+# # #         i_fac *= i
+# # #         out += m_fac // i_fac
+# #         c = choose_mod(m, i, MOD)
+# #         out = (out + c) % MOD
+# #     print(m, n)
+#     return out % p
