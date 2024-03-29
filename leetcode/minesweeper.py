a6ee6fd5c9d02d60d3ed4daab12675f47f46f8d3
Osher Lerner <osherler@gmail.com>
2024-03-28 18:14:03 -0700

    working on new Map approach, kinda works

--- leetcode/minesweeper.py
+++ leetcode/minesweeper.py
@@ -1,361 +1,184 @@
 # https://www.codewars.com/kata/57ff9d3b8f7dda23130015fa/train/pythonfrom preloaded import open
-import copy
+
+from preloaded import open
 
 def solve_mine(map, n):
-    map = [r.split(' ') for r in map.split('\n')]
-    h,w = len(map), len(map[0])
-    fs, all_qs = get_frontiers(map)
-    for f in fs:
-        print(".....", f.adj_qs)
+    m = Map(map, debug=False)
+    while (q := m.find_empty()) is not None:
+#         print("map", q)
+#         print(m)
+        num = open(q[0], q[1])
+        m.mark_opened(q, num)
+    s = str(m)
+    if '?' not in s:
+        return s
+    return '?'    
+
+class Map:
+    def __init__(self, map, debug=False):
+        self.map = [[c for c in r.split(' ')] for r in map.split('\n')]
+        self.h, self.w = len(self.map), len(self.map[0])
+#         self.qs = []
+        self.marked_stack = []
+        self.debug = debug
+        
+    def mark_empty(self, q):
+        if self.map[q[0]][q[1]] != '?':
+            print("\n\n--------BAD EMPTY--------\n\n")
+            print(q, self.qs)
+            1/0
+        if self.debug:
+            print("marking empty", q)
+            print(self)
+        self.map[q[0]][q[1]] = 'o'
+        self.marked_stack.append(q)
     
-#     print(fs)
-    # coding and coding...
-    q = "INIT"
-    while q is not None:
-        [print(' '.join(r)) for r in map]
-        for f in fs:
-            print(f)
-            q = f.find_safe_q()
-#             print("_", q)
-            if q is not None:
-                num = open(q[0], q[1])
-                all_qs.remove(q)
-                print(q, num)
-                map[q[0]][q[1]] = str(num)
-                adj_qs = [loc for loc in neighbors(q,h,w) if loc in all_qs]
-                f.update_opened(q, num, adj_qs)
-#                 print(".....", f.adj_qs)
-                for i in reversed(range(len(fs))):
-                    f2 = fs[i]
-                    if f2 is f:
-                        continue
-#                     print(q, i, f2.ns, f2.has_q(q))
-                    if f2.has_q(q):
-                        print("before", fs, f.ns)
-                        f2.update_opened(q, num, adj_qs)
-                        f.merge(f2)
-                        fs.remove(f2)
-                        print("after", fs, f.ns)
-                break
-#     q = any(f.find_safe_q() for f in fs)
-#         print(q)
-#         num = open(q[0], q[1])
-        
-#         q_fs = [f for f in fs if f.has_q(q)]
-    assert q != "INIT"
-    print("NONE")
-#     open(0,1)
-    [print(' '.join(r)) for r in map]
-    if q is None:
-        return '?'
+    def mark_bomb(self, q):
+        if self.map[q[0]][q[1]] != '?':
+            print("\n\n--------BAD BOMB--------\n\n")
+            1/0
+        if self.debug:
+            print("marking bomb", q)
+            print(self)
+        self.map[q[0]][q[1]] = 'x'
+        self.marked_stack.append(q)
 
-class Frontier:
-    def __init__(self, qs, ns, adj_ns, adj_qs):
-        self.qs, self.ns = qs, ns
-        self.adj_ns, self.adj_qs = adj_ns, adj_qs
-        self.empties = set()
-        self.bombs = set()
+    def unmark(self, level):
+        while len(self.marked_stack) > level:
+            q = self.marked_stack.pop()
+            self.map[q[0]][q[1]] = '?'
+#         qs = self.marked_stack[level:]
 
-        print(self.adj_qs)
-        print(self.empties)
-        print(self.bombs)
-        print('/')
-        for n in self.ns:
-            if len(self.adj_qs[n]) == self.ns[n]:
-                for q in copy.copy(self.adj_qs[n]):
-                    status = self.mark_bomb(q)
-#                     assert status != "INVALID"
-                    if status == "INVALID":
-                        print("\n\n Failed initialization bombs \n\n")
-            if self.ns[n] == 0:
-                for q in copy.copy(self.adj_qs[n]):
-                    status = self.mark_empty(q)
-#                     assert status != "INVALID"
-                    if status == "INVALID":
-                        print("\n\n Failed initialization bombs \n\n")
-#         Go over everything and see if it is forced. prune by seeing if during the search a q takes both values
-        
-        print(self.adj_qs)
-        print(self.empties)
-        print(self.bombs)
-        print("-\n")
-    
-    def merge(self, f2):
-        print(("-"*100 + "\n")*2)
-        if self.bombs.intersection(f2.empties) or self.empties.intersection(f2.bombs):
-            print('\n\n Failed intersection \n\n')
-        self.bombs.update(f2.bombs)
-        self.empties.update(f2.empties)
-        self.qs = [q for q in self.qs if q not in self.bombs and q not in self.empties]
-        self.qs += [q for q in f2.qs if q not in self.bombs and q not in self.empties and q not in self.qs]
-        for n in f2.adj_qs:
-            if n in self.adj_qs and self.adj_qs[n] != f2.adj_qs[n]:
-                print('\n\n Failed adj qs \n\n')
-            self.adj_qs[n] = f2.adj_qs[n]
-        for q in f2.adj_ns:
-            self.adj_ns[q] = self.adj_ns.get(q, []) + f2.adj_ns[q]
-        for n in f2.ns:
-            self.ns[n] = f2.ns[n]
+    def save_level(self):
+        return len(self.marked_stack)
 
-    def has_q(self, q):
-        if q == (4,3):
-            print("\n\n4,3")
-            print(self.qs)
-            print(self.empties)
-            print(self.bombs)
-        return q in self.qs or q in self.empties or q in self.bombs
-
-
-    def update_opened(self, q, num, adj_qs):
-        assert q not in self.bombs
-        # assert q in self.empties
-        # combine if q in multiple frontiers
-        if q in self.empties:
-            self.empties.remove(q)
-        else:
-            print("q not in empties")
-        if q in self.qs:
-            print("q in qs")
-            self.qs.remove(q)
-            for n in self.adj_ns[q]:
-                self.adj_qs[n].remove(q)
-        self.ns[q] = num
-#         del self.adj_ns[q]
-        self.adj_qs[q] = []
-        for q2 in adj_qs:
-            if q2 not in self.empties and q2 not in self.bombs:
-                if q2 not in self.qs:  
-                    self.qs.append(q2)
-                    self.adj_ns[q2] = []
-                self.adj_qs[q].append(q2)
-            if q not in self.adj_ns[q2]:
-                self.adj_ns[q2].append(q)
-                
-
-
-
-
-
-    # mark off 100% certain bombs?
-
-    def find_safe_q(self): # remember None answer if not changed. changed on merge or update_opened
-        if self.empties:
-            return next(iter(self.empties))
-
-        save = self.get_save()
-        for q in self.qs:
-            print("\n\n\n pre mark")
-            print(self.adj_qs)
-            print(self.ns)
-            print(self.empties, self.bombs, self.qs)
-            status = self.mark_bomb(q)
-            print("\n\n\n post mark")
-            print(self.adj_qs)
-            print(self.ns)
-            print(self.empties, self.bombs, self.qs)
-            if status == "INVALID" or not self.feasible():
-                if q == (1, 5):
-                    print(self.adj_qs)
-                    print(self.ns)
-                    print(status)
-#                     print(self.feasible(pr=True))
-                self.reset_to_save(save)
-                status = self.mark_empty(q)
-                if status == "INVALID" or not self.feasible():
-                    print("FAILED empty\n\n\n")
-                    print(q)
-                    print(self.qs)
-                    print(self.empties)
-                    print(self.bombs)
-                    print("__")
-                    print(self.adj_qs)
-                    print(self.ns)
-                    print(status)
-#                     print(self.feasible(pr=True))
-                    assert False
-                return q
-            self.reset_to_save(save)
-        return None
-
-    def feasible(self, pr=False):
-        if len(self.qs) == 0:
-            return all(self.ns[n] == 0 for n in self.ns)
-
-        q = self.qs[0]       
-        old_adjs = copy.deepcopy(self.adj_qs)
-        save = self.get_save()
-        status = self.mark_empty(q)
-        if status != "INVALID" and self.feasible(pr=pr):
-            if pr: print(q, "empty", status, "success")
-            self.reset_to_save(save)
-            return True
-        self.reset_to_save(save)
-
-        save = self.get_save()
-        status = self.mark_bomb(q)
-        if status != "INVALID" and self.feasible(pr=pr):
-            if pr: print(q, "bomb", status, "success")
-            self.reset_to_save(save)
-            return True
-        self.reset_to_save(save) # or just unmark marked qs
-
-        if pr:
-            print(q, "failed")
-            print('\t', self.qs)          
-            print('\t', self.empties, self.bombs)
-            print('\t', self.ns)
-            print('\t', {n: len(self.adj_qs[n]) for n in self.ns})
-            print('\t', {n: self.adj_qs[n] for n in self.ns})
-            print('\t', {n: old_adjs[n] for n in self.ns})
-        
+    def feasible(self):
+    #     if ns:
+    #         for n in ns:
+    #             if map[n] < len(q_neighbors(n))
+        level = self.save_level()
+        q = self.get_border_q()
+        if q is None:
+            if self.debug:
+                print("NONE", q)
+                print(self)
+            return self.check_filled()
+            
+        assert self.map[q[0]][q[1]] == '?'
+    #     queue += neighbor_qs
+        self.mark_bomb(q)
+        valid = self.propagate(q) and self.feasible()
+        self.unmark(level)
+        if valid: return True
+        self.mark_empty(q)
+        valid = self.propagate(q) and self.feasible()
+        self.unmark(level) # maybe skip?
+        if valid: return True
         return False
+        
+#         if self.mark_bomb(q) and self.feasible():
+#             self.unmark(level)
+#             return True
+#         else:
+#             self.unmark(level)
+#             if self.mark_empty(q) and self.feasible():
+#                 self.unmark(level)
+#                 return True
+#             else:
+#                 self.unmark(level)
+#                 return False
 
-    def mark_empty(self, q): # does this preserve our constraint len(adj_qs[n]) > ns[n] and ns[n] >= 0
-        if q in self.empties: return "OK"
-        if q in self.bombs: return "INVALID"
-        before = self.check()
-                    
-        self.qs.remove(q)
-        self.empties.add(q)
-        for n in self.adj_ns[q]:
-            self.adj_qs[n].remove(q)
-        after = self.check()
-        if before != after:
-            print("\n"*3 + "^^^.."*100 + "\n")
-            print("q", q)
-            for n in self.adj_ns[q]:
-                print(q in self.adj_qs[n], self.adj_qs[n])
-            print(self.adj_qs)
-            print(self.qs)
-        for n in self.adj_ns[q]:
-            if len(self.adj_qs[n]) < self.ns[n]:
-                print("small list")
-                return "INVALID"
-            if len(self.adj_qs[n]) == self.ns[n]:
-                for b in copy.copy(self.adj_qs[n]):
-                    status = self.mark_bomb(b)
-                    if status == "INVALID": return status
-        return "OK"
+    def propagate(self, q): # mutate map. Return False if invalid
+        next_qs = set()
+    #     if map[q] == 'x':
+        for n in self.ns_near(q):
+            leftover = int(self.map[n[0]][n[1]]) - self.num_bombs_near(n)
+            n_qs = self.qs_near(n)
+            if leftover < 0 or leftover > len(n_qs):
+                return False
+            elif leftover == 0:
+                for next_q in n_qs:
+                    self.mark_empty(next_q)
+                next_qs.update(n_qs)
+            elif leftover == len(n_qs):
+                for next_q in n_qs:
+                    self.mark_bomb(next_q)
+                    # next_qs.add(next_q)
+                next_qs.update(n_qs)
+    #         else:
+    #             queue += next_qs
+    # optimize by collecting ns?
+        return all(self.propagate(next_q) for next_q in next_qs)
 
-    def mark_bomb(self, q): # mark and propagate. if invalid, break (garbled state) 
-        if q in self.empties: return "INVALID"
-        if q in self.bombs: return "OK"
-        before = self.check()
-        self.qs.remove(q)
-        self.bombs.add(q)
-        for n in self.adj_ns[q]:
-            if q not in self.adj_qs[n]:
-                print('\n\n\n &&&&&--------------&&&&&')
-                print(n, q)
-            self.adj_qs[n].remove(q)
-            self.ns[n] -= 1
-        after = self.check()
-        if before != after:
-            print("\n"*3 + "^^^.."*100 + "\n")
-            print(before, after)
-            print("q", q)
-            print(self.adj_ns[q])
-            for n in self.adj_ns[q]:
-                print(q in self.adj_qs[n], n, self.adj_qs[n])
-            print(self.adj_qs)
-            print(self.qs)
-        for n in self.adj_ns[q]:
-            if self.ns[n] < 0:
-                print("below zero")
-                return "INVALID"
-            if self.ns[n] == 0:
-                for b in copy.copy(self.adj_qs[n]):
-                    status = self.mark_empty(b)
-                    if status == "INVALID": return status
-        return "OK"
-
-    def get_save(self):
-        saved_attrs = ["qs", "empties", "bombs", "adj_qs", "ns", "adj_ns"]
-        save = {attr: copy.deepcopy(getattr(self, attr)) for attr in saved_attrs}
-        return save
+    def check_filled(self):
+        return True
     
-    def reset_to_save(self, save):
-        for attr, val in save.items():
-            setattr(self, attr, val)
+    def ns_near(self, q):
+        for n in self.neighbors(q):
+            if n[0] >= len(self.map) or n[1] >= len(self.map[n[0]]):
+                print(q, n, self.h, self.w)
+        return [n for n in self.neighbors(q) if self.map[n[0]][n[1]].isdigit()]
 
-    def check(self):
-        for N in self.adj_qs:
-            for Q in self.adj_qs[N]:
-                if Q not in self.qs:
-                    return "ALERT"
-        return "all clear"
-def get_frontiers(map):
-    h, w = len(map), len(map[0])
+    def qs_near(self, n):
+        return [q for q in self.neighbors(n) if self.map[q[0]][q[1]] == '?']
 
-    comps, comp_map = {}, {}
-    adj_qs, ns = {}, {}
-    all_qs = []
-    c_id = 0
-    for i in range(h):
-        for j in range(w):
-            loc = (i,j)
-            if map[loc[0]][loc[1]] == '?':
-                all_qs.append(loc)
-                continue
+    def num_bombs_near(self, n):
+        return len([q for q in self.neighbors(n) if self.map[q[0]][q[1]] == 'x'])
 
-            if loc in comp_map:
-                ci = comp_map[loc]
-            else:
-                ci = (c_id := c_id + 1)
-                comps[ci] = [loc]
-                comp_map[loc] = ci
-            loc_adj_qs = []
-            for n in neighbors(loc, h, w):
-                if map[n[0]][n[1]] == '?':
-                    loc_adj_qs.append(n)
-                else:
-                    if n in comp_map:
-                        if comp_map[n] != ci:
-                            n_comp = comps.pop(comp_map[n])
-                            for u in n_comp:
-                                comp_map[u] = ci
-                            comps[ci].extend(n_comp)
-                    else:
-                        comp_map[n] = ci
-                        comps[ci].append(n)
-            if loc_adj_qs:
-                adj_qs[loc] = loc_adj_qs
-                # ns[loc] = int(map[loc[0]][loc[1]])
-            else:
-                assert int(map[loc[0]][loc[1]]) == 0
-#     print("NS__", adj_qs[(2,3)])
-    fs = []
-    for ci,comp in comps.items():
-        c_qs = []
-        c_ns = {} # coord to num
-        c_adj_ns = {} # q coord to neighboring n coords
-        c_adj_qs = {}
-        for loc in comp:
-            if loc not in adj_qs:
-                continue # not on border
-            c_adj_qs[loc] = adj_qs[loc]
-            for q in adj_qs[loc]:
-                if q not in c_adj_ns:
-                    if q == (1,5):
-                         print("\t_________\n"*3)
-                    c_adj_ns[q] = []
-                    c_qs.append(q)
-                c_adj_ns[q].append(loc)
-            c_ns[loc] = int(map[loc[0]][loc[1]])
-        if (1,5) in c_adj_ns:
-            print("\t_________\n"*3, c_adj_ns[(1,5)])
-        fs.append(Frontier(c_qs, c_ns, c_adj_ns, c_adj_qs))
+    def get_border_q(self):
+        for i in range(self.h):
+            for j in range(self.w):
+                if self.map[i][j] == '?' and self.ns_near((i,j)):
+                    return (i, j)
 
-    return fs, all_qs
+    def get_border_qs(self):
+        return [(i, j) for i in range(self.h) for j in range(self.w) if self.map[i][j] == '?' and self.ns_near((i,j))]
+#         for i in range(self.h):
+#             for j in range(self.w):
+#                 if self.map[i][j] == '?' and self.ns_near((i,j)):
+#                     qs.append((i, j))
 
-def neighbors(loc, h, w):
-    ns = []
-    for di in [-1,0,1]:
-        ni = loc[0] + di
-        if ni < 0 or ni >= h: continue
-        for dj in [-1,0,1]:
-            if di==0 and dj==0: continue
-            nj = loc[1] + dj
-            if nj < 0 or ni >= w: continue
-            ns.append((ni,nj))
-    return ns
+    def neighbors(self, loc):
+        ns = []
+        for di in [-1,0,1]:
+            ni = loc[0] + di
+            if ni < 0 or ni >= self.h: continue
+            for dj in [-1,0,1]:
+                if di==0 and dj==0: continue
+                nj = loc[1] + dj
+                if nj < 0 or nj >= self.w: continue
+                ns.append((ni,nj))
+        return ns
+
+    def find_empty(self):
+        level = self.save_level()
+        for i in range(self.h):
+            for j in range(self.w):
+                q = (i, j)
+                if self.map[i][j] == 'o': return q
+                if self.map[i][j] == '?' and self.ns_near(q):
+                    if self.debug: print('\n\n\nTrying', q)
+                    self.mark_bomb(q)
+                    valid = self.propagate(q) and self.feasible()
+                    self.unmark(level)
+                        # don't reset
+#                     if (i,j) == (1,1): self.debug = False
+
+#                     print(i, j, self.ns_near((i,j)))
+#                     for n in self.ns_near((i,j)):
+#                         print(n, self.map[n[0]][n[1]])
+#                     print("empty stat", self.mark_empty((i,j)))
+                    if not valid:
+                        self.mark_empty(q)
+                        if not self.propagate(q):
+                            print("CONTRADICTION")
+                            1/0
+                        return q
+
+    def mark_opened(self, loc, num):
+        print("marked open", loc, num, "was", self.map[loc[0]][loc[1]])
+        self.map[loc[0]][loc[1]] = str(num)
+
+    def __str__(self):
+        return '\n'.join(' '.join(r) for r in self.map)
+    
