bd7cf2b3ff43aafede28a47ac3873bf9efdccfcc
Osher Lerner <osherler@gmail.com>
2024-04-03 02:24:34 -0700

    working half step and plus correction, not fast

--- break_pieces.py
+++ break_pieces.py
@@ -1,48 +1,69 @@
 # # https://www.codewars.com/kata/591f3a2a6368b6658800020e/train/python
 
-
+import math
 USE_BREAK_DISPLAY = True        # to get more details in the console when a test fails
 
-
 def break_evil_pieces(shape):
+    if not shape: return []
+#     if shape[0] == '\n': shape = shape[1:]
     shape = shape.split('\n')
-#     shape = [' '.join(r) for r in shape]
     cs = find_components(shape)
     return [process_component(shape, c) for c in cs]
 
-def expand_shape(shape):
-    for i in range(len(shape)):
-        for 
+def is_empty(shape, n):
+    if n[0] % 1 == 0 and n[1] % 1 == 0:
+        if shape[int(n[0])][int(n[1])] != ' ':
+            return False
+    elif n[0] % 1 != 0 and n[1] % 1 == 0:
+        ceil, floor = math.ceil(n[0]), math.floor(n[0])
+        if shape[ceil][int(n[1])] in '|+' and shape[floor][int(n[1])] in '|+':
+            return False
+    elif n[0] % 1 == 0 and n[1] % 1 != 0:
+        ceil, floor = math.ceil(n[1]), math.floor(n[1])
+        if shape[int(n[0])][ceil] in '-+' and shape[int(n[0])][floor] in '-+':
+            return False
+    return True
+
 def find_component(shape, seed):
     component = set()
+    on_border = False
     queue = {seed}
     while queue:
         v = queue.pop()
-        if v[0] == 0 or v[0] == len(shape) or v[1] == 0 or v[1] == len(shape[v[0]]):
-            return None
+        ceil, floor = math.ceil(v[0]), math.floor(v[0])
+        if v[0] == 0 or v[0] == len(shape)-1 or v[1] == 0 or v[1] == len(shape[ceil])-1 or v[1] == len(shape[floor])-1:
+            on_border = True
         component.add(v)
-        ns = [(v[0]+di,v[1]+dj) for di in [-1,0,1] for dj in [-1,0,1]]
-        ns = [n for n in ns if n!=v and 0<=n[0]<len(shape) and 0<=n[1]<len(shape[n[0]])]
+        ns = [(v[0]+di,v[1]+dj) for di in [-0.5,0,0.5] for dj in [-0.5,0,0.5]]
         for n in ns:
+            if n == v: continue
+            ceil, floor = math.ceil(n[0]), math.floor(n[0])
+            if not (0<=n[0]<=len(shape)-1 and 0<=n[1]<=min(len(shape[ceil]), len(shape[floor]))-1):
+#                 print(v, n, ceil, floor, len(shape))
+                on_border = True
+                continue
             if n in component: continue
-            if shape[n[0]][n[1]] == ' ':
+            if is_empty(shape, n):
                 queue.add(n)
             else:
                 component.add(n)
-    return component
+    return component, on_border
 
 def find_components(shape):
     seen = set()
     components = []
-    for i in range(len(shape)):
-        for j in range(len(shape[i])):
+    for i in [x/2 for x in range(0,2*len(shape)-1)]:
+        ceil, floor = math.ceil(i), math.floor(i)
+        for j in [x/2 for x in range(0,2*min(len(shape[ceil]),len(shape[floor]))-1)]:
             loc = (i,j)
             if loc in seen: continue
-            if shape[loc[0]][loc[1]] != ' ': continue
-            component = find_component(shape, loc)
-            if component is None: continue
+            if not is_empty(shape, loc): continue
+            component, on_border = find_component(shape, loc)
             seen |= component
-            components.append(component)
+            if not on_border:
+                component = set((int(i),int(j)) for i,j in component if i%1==0 and j%1==0)
+                components.append(component)
+
     return components
 
 def find_first_col(s):
@@ -53,10 +74,58 @@ def find_first_col(s):
             if s[i][j] != ' ':
                 return j
 
+def find_first_corner(s):
+    for i in range(len(s)):
+        for j in range(len(s[i])):
+            if s[i][j] == '+':
+                return (i,j)
+dir = {"right": (0,1), "left": (0,-1), "up": (-1,0), "down": (1,0)}
+turn_right = {"right": "down", "down": "left", "left": "up", "up": "right"}
+turn_left = {"right": "up", "down": "right", "left": "down", "up": "left"}
+chars = {"right": '-', "left": '-', "up": '|', "down": '|'}
+def move(c,heading):
+    return (c[0] + dir[heading][0], c[1] + dir[heading][1])
+
+def unplus(s, start, outer):
+    plus_to_change = {}
+    plus_to_keep = set()
+    visited = {start}
+    heading = "right"
+    c = start
+    for _ in range(3):
+        c_next = move(c, heading)
+        if (0<=c_next[0]<len(s) and 0<=c_next[1]<len(s[c_next[0]])):
+            if s[c_next[0]][c_next[1]] in ("+"+chars[heading]):
+                break
+        heading = turn_right[heading] if outer else turn_left[heading]
+    c = c_next
+    while True:
+        while s[c[0]][c[1]] in '-|':
+            c = move(c, heading)
+
+        if s[c[0]][c[1]] != '+':
+            print(c, heading, s[c[0]][c[1]])
+        if c == start: break
+        old_heading = heading
+        heading = turn_right[heading] if outer else turn_left[heading]
+        for _ in range(3):
+            c_next = move(c, heading)
+            if (0<=c_next[0]<len(s) and 0<=c_next[1]<len(s[c_next[0]])):
+                if s[c_next[0]][c_next[1]] in ("+"+chars[heading]):
+                    break
+            heading = turn_left[heading] if outer else turn_right[heading]
+        if heading == old_heading:
+            plus_to_change[c] = chars[heading]
+        else:
+            plus_to_keep.add(c)
+        visited.add(c)
+        c = move(c, heading)
+    for c in plus_to_change:
+        if c in plus_to_keep: continue
+        s[c[0]] = s[c[0]][:c[1]] + plus_to_change[c] + s[c[0]][c[1]+1:]
+    return visited
+# turn to string, remove whitespace, convert useless + to -|
 def process_component(shape, component):
-    #turn to string
-    #remove the injected whitespace
-    #convert useless + to -|
     s = []
     for i in range(len(shape)):
         row = ""
@@ -68,28 +137,103 @@ def process_component(shape, component):
         row = row.rstrip()
         if row:
             s.append(row)
-            print(row)
     
     j = find_first_col(s)
-    print(j)
     s = [r[j:] for r in s]
+    visited = set()
+    outer = True
     for i in range(len(s)):
         for j in range(len(s[i])):
             if s[i][j] != '+': continue
-            hor, ver = 0, 0
-            if 0<=i+1<len(s) and 0<=j<len(s[i+1]) and s[i+1][j] == '|':
-                ver += 1
-            if 0<=i-1<len(s) and 0<=j<len(s[i-1]) and s[i-1][j] == '|':
-                ver += 1
-            if 0<=j+1<len(s[i]) and s[i][j+1] == '-':
-                hor += 1
-            if 0<=j-1<len(s[i]) and s[i][j-1] == '-':
-                hor += 1
-            if hor == 0 and ver == 2:
-                s[i] = s[i][:j] + '|' + s[i][j+1:]
-            elif hor == 2 and ver == 0:
-                s[i] = s[i][:j] + '-' + s[i][j+1:]
+            start = (i,j)
+            if start in visited: continue
+            visited |= unplus(s, start, outer)
+            outer = False
     return '\n'.join(''.join(r) for r in s)
+
+# def break_evil_pieces(shape):
+#     shape = shape.split('\n')
+# #     shape = [' '.join(r) for r in shape]
+#     cs = find_components(shape)
+#     return [process_component(shape, c) for c in cs]
+
+# def find_component(shape, seed):
+#     component = set()
+#     queue = {seed}
+#     while queue:
+#         v = queue.pop()
+#         if v[0] == 0 or v[0] == len(shape) or v[1] == 0 or v[1] == len(shape[v[0]]):
+#             return None
+#         component.add(v)
+#         ns = [(v[0]+di,v[1]+dj) for di in [-1,0,1] for dj in [-1,0,1]]
+#         ns = [n for n in ns if n!=v and 0<=n[0]<len(shape) and 0<=n[1]<len(shape[n[0]])]
+#         for n in ns:
+#             if n in component: continue
+#             if shape[n[0]][n[1]] == ' ':
+#                 queue.add(n)
+#             else:
+#                 component.add(n)
+#     return component
+
+# def find_components(shape):
+#     seen = set()
+#     components = []
+#     for i in range(len(shape)):
+#         for j in range(len(shape[i])):
+#             loc = (i,j)
+#             if loc in seen: continue
+#             if shape[loc[0]][loc[1]] != ' ': continue
+#             component = find_component(shape, loc)
+#             if component is None: continue
+#             seen |= component
+#             components.append(component)
+#     return components
+
+# def find_first_col(s):
+#     for j in range(len(s[0])):
+#         for i in range(len(s)):
+#             if j >= len(s[i]):
+#                 return j
+#             if s[i][j] != ' ':
+#                 return j
+
+# def process_component(shape, component):
+#     #turn to string
+#     #remove the injected whitespace
+#     #convert useless + to -|
+#     s = []
+#     for i in range(len(shape)):
+#         row = ""
+#         for j in range(len(shape[i])):
+#             if (i,j) in component:
+#                 row += shape[i][j]
+#             else:
+#                 row += ' '
+#         row = row.rstrip()
+#         if row:
+#             s.append(row)
+#             print(row)
+    
+#     j = find_first_col(s)
+#     print(j)
+#     s = [r[j:] for r in s]
+#     for i in range(len(s)):
+#         for j in range(len(s[i])):
+#             if s[i][j] != '+': continue
+#             hor, ver = 0, 0
+#             if 0<=i+1<len(s) and 0<=j<len(s[i+1]) and s[i+1][j] == '|':
+#                 ver += 1
+#             if 0<=i-1<len(s) and 0<=j<len(s[i-1]) and s[i-1][j] == '|':
+#                 ver += 1
+#             if 0<=j+1<len(s[i]) and s[i][j+1] == '-':
+#                 hor += 1
+#             if 0<=j-1<len(s[i]) and s[i][j-1] == '-':
+#                 hor += 1
+#             if hor == 0 and ver == 2:
+#                 s[i] = s[i][:j] + '|' + s[i][j+1:]
+#             elif hor == 2 and ver == 0:
+#                 s[i] = s[i][:j] + '-' + s[i][j+1:]
+#     return '\n'.join(''.join(r) for r in s)
 
 
 
