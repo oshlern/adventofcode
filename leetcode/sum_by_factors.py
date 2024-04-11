5a02648a19815d6ef0a395f5cc82e1eb0672fd57
Osher Lerner <osherler@gmail.com>
2024-04-10 22:10:45 -0700

    working sum by factors

--- leetcode/sum_by_factors.py
+++ leetcode/sum_by_factors.py
@@ -1,41 +1,63 @@
+# https://www.codewars.com/kata/54d496788776e49e6b00052f/train/python
+
 def sum_for_list(lst):
-#     ps = [2,3]
-#     for k in range(3,int(max(I)**0.5),2):
-    print(lst)
-#     print(max(map(abs,lst))**0.5)
-    print(max(map(abs,lst))//2)
+    ns = list(map(abs, lst))
     ps, out = [], []
-#     for k in range(2,int(max(map(abs,lst))**0.5)+1):
-#     for k in range(2,max(map(abs,lst))):
-#     ps = []
-#     fs = [set() for _ in lst]
-    ns = list(map(abs,lst))
     k = 2
     while k*k <= max(ns):
-        if all(k%p != 0 for p in ps):
-            ps.append(k)
-            ls = []
-            for i in range(len(ns)):
-                if ns[i] % k == 0:
-#                     fs[i].add(k)
-                    ls.append(lst[i])
-                    while ns[i] % k == 0:
-                        ns[i] //= k
-            if ls:
-                out.append([k, sum(ls)])
+        ls = []
+        for i in range(len(ns)):
+            if ns[i] % k == 0:
+                ls.append(lst[i])
+                while ns[i] % k == 0:
+                    ns[i] //= k
+        if ls:
+            out.append([k, sum(ls)])
         k += 1
-    for k in set(ns):
+    for k in sorted(set(ns)):
         if k != 1:
-            
+            out.append([k, sum(l for l in lst if l%k == 0)])
     return out
-#     out = [(p, [n for n,f in zip(lst,fs) if p in f]) for p in ps]
-#     out = in range(lenfor p in ps]
-#     ps = [2]
-#     if (I := [i for i in lst if i%2 == 0]):
-#         out.append([2, sum(I)])
-#     for k in range(3,max(map(abs,lst)),2):
+
+
+# def sum_for_list(lst):
+# #     ps = [2,3]
+# #     for k in range(3,int(max(I)**0.5),2):
+#     print(lst)
+# #     print(max(map(abs,lst))**0.5)
+#     print(max(map(abs,lst))//2)
+#     ps, out = [], []
+# #     for k in range(2,int(max(map(abs,lst))**0.5)+1):
+# #     for k in range(2,max(map(abs,lst))):
+# #     ps = []
+# #     fs = [set() for _ in lst]
+#     ns = list(map(abs,lst))
+#     k = 2
+#     while k*k <= max(ns):
 #         if all(k%p != 0 for p in ps):
 #             ps.append(k)
-#             if (I := [i for i in lst if i%k == 0]):
-#                 out.append([k, sum(I)])
+#             ls = []
+#             for i in range(len(ns)):
+#                 if ns[i] % k == 0:
+# #                     fs[i].add(k)
+#                     ls.append(lst[i])
+#                     while ns[i] % k == 0:
+#                         ns[i] //= k
+#             if ls:
+#                 out.append([k, sum(ls)])
+#         k += 1
+#     for k in set(ns):
+#         if k != 1:
+            
 #     return out
+# #     out = [(p, [n for n,f in zip(lst,fs) if p in f]) for p in ps]
+# #     out = in range(lenfor p in ps]
+# #     ps = [2]
+# #     if (I := [i for i in lst if i%2 == 0]):
+# #         out.append([2, sum(I)])
+# #     for k in range(3,max(map(abs,lst)),2):
+# #         if all(k%p != 0 for p in ps):
+# #             ps.append(k)
+# #             if (I := [i for i in lst if i%k == 0]):
+# #                 out.append([k, sum(I)])
+# #     return out
