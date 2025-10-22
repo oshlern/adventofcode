8860fba09fa15579cd6221940e32469095803b6c
Osher Lerner <osherler@gmail.com>
2025-10-22 04:35:10 -0700

    fast vowel check

--- leetcode/vowel_checker.py
+++ leetcode/vowel_checker.py
@@ -1,8 +1,26 @@
 # https://leetcode.com/problems/vowel-spellchecker/submissions/1808445513/
 VOWELS = "aeiou"
+devowelize = lambda w: "".join("." if c in VOWELS else c for c in w.lower())
 
 class Solution:
     def spellchecker(self, wordlist: List[str], queries: List[str]) -> List[str]:
+        ws = set()
+        lws = dict()
+        vws = dict()
+        for w in wordlist:
+            lw = w.lower()
+            vw = devowelize(w)
+            ws.add(w)
+            if lw not in lws:
+                lws[lw] = w
+            if vw not in vws:
+                vws[vw] = w
+
+        return [q if q in ws else (l if (l := lws.get(q.lower())) else (v if (v := vws.get(devowelize(q))) else "")) for q in queries]
+        # return [self.spellcheck(wordlist, query) for query in queries]
+
+
+    def old_spellchecker(self, wordlist: List[str], queries: List[str]) -> List[str]:
         return [self.spellcheck(wordlist, query) for query in queries]
 
 
