95720d4b838eea5b58596ba98e395b06a77b4b08
Osher Lerner <osherler@gmail.com>
2025-10-23 22:55:50 -0700

    working linked list addition

--- leetcode/add_two_numbers.cpp
+++ leetcode/add_two_numbers.cpp
@@ -10,49 +10,55 @@
  * };
  */
 class Solution {
-public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* L1 = reverse(l1);
-        ListNode* L2 = reverse(l2);
+    public:
+        ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+            ListNode* L1 = reverse(l1);
+            ListNode* L2 = reverse(l2);
 
+            int n;
+            int carry = 0;
+            ListNode* out = nullptr;
 
-        int n;
-        int carry = 0;
-        ListNode* out = nullptr;
+            // Add two numbers
+            while (L1 != nullptr && L2 != nullptr) {
+                n = L1->val + L2->val + carry;
+                carry = n / 10;
+                out = new ListNode(n % 10, out);
+                L1 = L1->next;
+                L2 = L2->next;
+            }
 
-        while (L1 != nullptr && L2 != nullptr) {
-            n = L1->val + L2->val + carry;
-            carry = n / 10;
-            out = new ListNode(n % 10, out);
-            L1 = L1->next;
-            L2 = L2->next;
+            // Add one number
+            ListNode* L;
+            if (L1 == nullptr) {
+                L = L2;
+            } else {
+                L = L1;
+            }
+            while (L != nullptr) {
+                n = L->val + carry;
+                carry = n / 10;
+                out = new ListNode(n % 10, out);
+                L = L->next;
+            }
+
+            // Add zero numbers
+            while (carry != 0) {
+                n = carry;
+                carry = n / 10;
+                out = new ListNode(n % 10, out);
+            }
+
+            return out;
         }
-        ListNode* L;
-        if (L1 == nullptr) {
-            L = L1;
-        } else {
-            L = L2;
+
+
+        ListNode* reverse(ListNode* l) {
+            ListNode* L = nullptr;
+            while (l != nullptr) {
+                L = new ListNode(l->val, L);
+                l = l->next;
+            }
+            return L;
         }
-        while (L != nullptr) {
-            n = L->val + carry;
-            carry = n / 10
-            out = new ListNode(n % 10, out);
-            L = L->next
-        }
-        return out;
-    }
-
-    // ListNode* add(ListNode* L1, ListNode* L2, int carry) {
-
-
-    // }
-
-    ListNode* reverse(ListNode* l) {
-        ListNode* L = nullptr;
-        while (l != nullptr) {
-            L = new ListNode(l->val, L);
-            l = l->next;
-        }
-        return L;
-    }
-};
+    };
