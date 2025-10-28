b7a761f04a6ca3ea26fa6201fad1a95852ea84b3
Osher Lerner <osherler@gmail.com>
2025-10-27 23:49:25 -0700

    left justification

--- leetcode/text_justification.py
+++ leetcode/text_justification.py
@@ -2,45 +2,39 @@
 
 class Solution:
     def fullJustify(self, words: List[str], maxWidth: int) -> List[str]:
-        i = 0
-        line = []
-        while sum(len(w)+1 for w in line) - 1 <= maxWidth:
-            line.append(words[i])
-            i += 1
-            if
+        out = []
+        while words:
+            line, i = self.build_line(words, maxWidth)
+            print("line", line, i)
+            out.append(line)
+            words = words[i:]
+        return out
 
-    def build_line(words, maxWidth):
+    def build_line(self, words, maxWidth):
         n_chars = len(words[0])
         i = 1
         while i < len(words):
             word = words[i]
             i += 1
             if n_chars + 1 + len(word) > maxWidth:
+                i -= 1
                 break
+            n_chars += 1 + len(word)
 
-        if i >= len(words):
+        ws = words[:i] # exclude last i
+        spaces = [" " for _ in range(i-1)]
 
-        # l_words = []
+        left_justify = i == 1 or i == len(words)
+        n_extra_spaces = maxWidth - n_chars
+        if not left_justify:
+            for j in range(maxWidth - n_chars):
+                spaces[j % len(spaces)] += " "
 
-        #     if i >= len(words):
-        #         break
-        #         return build_last_line(l_words, maxWidth), None
-        #     line.append(words[i])
-        #     i += 1
-        # l_words.pop()
-        # n_words = len(l_words)
-        # n_extra_spaces = maxWidth - sum(len(w) for w in l_words)
-        # slots = [" " for i in range(max(n_words-1, 1)]
-        # for i in range(n_spaces):
-        #     slots[i % len(slots)] += " "
-        # l = ""
-        # for i in range(n_words):
-        #     l += l_words[i]
-        #     if i < len(slots):
-        #         l += slots[i]
-        # return l
-
-
-
-
-    def line_len(line):
+        line = ""
+        for j in range(i):
+            line += ws[j]
+            if j < len(spaces):
+                line += spaces[j]
+        if left_justify:
+            line += " "*n_extra_spaces
+        return line, i
