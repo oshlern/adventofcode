262d28a289670545ea5722a8e9c062057b1a8fa5
Osher Lerner <osherler@gmail.com>
2025-11-14 15:17:39 +0900

    max perf solved

--- leetcode/max_performance_of_team.py
+++ leetcode/max_performance_of_team.py
@@ -1,17 +1,15 @@
-# https://leetcode.com/problems/maximum-performance-of-a-team/
+# https://leetcode.com/problems/maximum-performance-of-a-team/?source=submission-ac
+
+import heapq
 
 class Solution:
     def maxPerformance(self, n: int, speed: List[int], efficiency: List[int], k: int) -> int:
-        # argmax(sum(sp)*sum(ef))
-        # = argmax(ln(sum(sp)) + ln(sum(ef)))
-        # Es = list(zip(speed, efficiency))
-        # Ls = Es[:k]
-        Ss = speed[:k]
-        Es = efficiency[:k]
-        for i in range(k+1, n+1):
-            s = speed[i]
-            E = Es[i]
-            total_s = sum(Ss)
-            total_e = sum(Es)
-
-        print(Es)
+        Bs, tot_s, best = [], 0, 0
+        for i, eng in enumerate(sorted(list(zip(speed, efficiency)), key=lambda x: x[1], reverse=True)):
+            tot_s += eng[0]
+            heapq.heappush(Bs, eng[0])
+            if i >= k:
+                tot_s -= heapq.heappop(Bs)
+            if tot_s * eng[1] > best:
+                best = tot_s * eng[1]
+        return best % (10**9 + 7)
